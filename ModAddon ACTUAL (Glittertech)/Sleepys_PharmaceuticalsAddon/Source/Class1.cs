36086cd4a106e4e4a1e3c8261de25d8b6892da13
sleepy068 <58592380+sleepy068@users.noreply.github.com>
2024-03-18 16:24:40 +1100

    sync

---
+++ ModAddon ACTUAL (Glittertech)/Sleepys_PharmaceuticalsAddon/Source/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.Sound;
+using Verse.AI.Group;
+using RimWorld;
+using UnityEngine;
+
+namespace Sleepys_PharmaceuticalsAddon
+{
+    public class SLP_CompGiveInspiration : ThingComp
+    {
+        public SLP_CompProperties_GiveInspiration Props => (SLP_CompProperties_GiveInspiration)this.props;
+
+        public override void PrePostIngested(Pawn ingester)
+        {
+            if (ingester == null)
+                return;
+            InspirationDef SLP_inspiration = this.Props.SLP_inspiration;
+            if (SLP_inspiration != null)
+                ingester.mindState.inspirationHandler.TryStartInspiration(SLP_inspiration);
+        }
+    }
+
+    public class SLP_CompProperties_GiveInspiration : CompProperties
+    {
+        public InspirationDef SLP_inspiration;
+
+        public SLP_CompProperties_GiveInspiration() => this.compClass = typeof (SLP_CompGiveInspiration);
+    }
+
+}
